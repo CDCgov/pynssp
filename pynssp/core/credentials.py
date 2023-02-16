942661fb2770649feda08f164ec1c4de847e9f02
Azondekon <poj2@cdc.gov>
2023-02-16 14:44:58 -0500

    Update core submodule

---
+++ pynssp/core/credentials.py
@@ -0,0 +1,14 @@
+class Credentials:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+    def get_api_response(url):
+        pass
+
+    def get_api_data(url, fromCSV = False):
+        pass
+
+    def get_api_graph(url, file_ext = ".png"):
+        pass
+
---
+++ pynssp/core/token.py
@@ -0,0 +1,12 @@
+class Credentials:
+    def __init__(self, token):
+        self.token = token
+    
+    def get_api_response(url):
+        pass
+
+    def get_api_data(url, fromCSV = False):
+        pass
+
+    def get_api_graph(url, file_ext = ".png"):
+        pass
