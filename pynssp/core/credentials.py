b04544c7f416486c237a1cba94d1ee514db51ecd
Azondekon <poj2@cdc.gov>
2023-03-20 16:09:43 -0400

    Update core credentials submodule

--- pynssp/core/credentials.py
+++ pynssp/core/credentials.py
@@ -1,14 +1,26 @@
+import requests
+import json
+from pandas import json_normalize
+
 class Credentials:
     def __init__(self, username, password):
         self.username = username
         self.password = password
     
+
     def get_api_response(url):
-        pass
+        return requests.get(url)
+    
 
     def get_api_data(url, fromCSV = False):
-        pass
-
+        if not fromCSV:
+            response = self.get_api_response(url)
+            response_json = json.loads(response.content)
+            return json_normalize(response_json)
+        else:
+            pass
+    
+    
     def get_api_graph(url, file_ext = ".png"):
         pass
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ "pandas", "json", "requests" ]
 
 test_requirements = ['pytest>=3', ]
 
