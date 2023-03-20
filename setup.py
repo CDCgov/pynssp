dc238e5354bf8fc74e68ab0c09e5083fe8b85256
Azondekon <poj2@cdc.gov>
2023-03-20 16:49:09 -0400

    Update package dependency list

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = [ "pandas" ]
+requirements = [ "pandas", "cryptography" ]
 
 test_requirements = ['pytest>=3', ]
 
