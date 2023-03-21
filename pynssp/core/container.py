7edb07cec383ad84f7ae4cf1956d3d1ef2b4d9f5
Azondekon <poj2@cdc.gov>
2023-03-21 11:29:20 -0400

    Add container Class

---
+++ pynssp/core/container.py
@@ -0,0 +1,3 @@
+class NSSPContainer:
+    def __init__(self, value):
+        self.value = value
--- pynssp/core/credentials.py
+++ pynssp/core/credentials.py
@@ -2,19 +2,22 @@ import requests
 import json
 from pandas import json_normalize
 from cryptography.fernet import Fernet
+from pynssp.core.container import NSSPContainer
 
 class Credentials:
     def __init__(self, username, password):
-        self._k_ = Fernet(Fernet.generate_key())
-        self.username = self._k_.encrypt(username.encode())
-        self.password = self._k_.encrypt(password.encode())
+        self.__k = Fernet(Fernet.generate_key())
+        self.username = NSSPContainer(self.__k.encrypt(username.encode()))
+        self.password = NSSPContainer(self.__k.encrypt(password.encode()))
     
 
-    def get_api_response(url):
-        return requests.get(url)
+    def get_api_response(self, url):
+        auth = (self.__k.decrypt(self.username.value.encode()), self.__k.decrypt(self.password.value.encode()))
+        resp = requests.get(url, auth = auth)
+        return resp
     
 
-    def get_api_data(url, fromCSV = False):
+    def get_api_data(self, url, fromCSV = False):
         if not fromCSV:
             response = self.get_api_response(url)
             response_json = json.loads(response.content)
