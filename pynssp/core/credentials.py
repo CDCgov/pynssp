9696a18ecabc07be770a88fa4eea40b8ae34fe2b
Azondekon <poj2@cdc.gov>
2023-03-23 14:50:50 -0400

    Add code to get_api_data and get_api_graph methods

--- pynssp/core/credentials.py
+++ pynssp/core/credentials.py
@@ -1,8 +1,10 @@
 import requests
 import json
-from pandas import json_normalize
+import io
+from pandas import json_normalize, read_csv
 from cryptography.fernet import Fernet
 from pynssp.core.container import NSSPContainer
+from tempfile import TemporaryFile
 
 class Credentials:
     def __init__(self, username, password):
@@ -12,20 +14,28 @@ class Credentials:
     
 
     def get_api_response(self, url):
-        auth = (self.__k.decrypt(self.username.value.encode()), self.__k.decrypt(self.password.value.encode()))
-        resp = requests.get(url, auth = auth)
-        return resp
+        auth = (self.__k.decrypt(self.username.value.encode()), 
+                self.__k.decrypt(self.password.value.encode()))
+        response = requests.get(url, auth = auth)
+        print("%s: %s".format(response.status_code, response.reason))
+        if response.status_code == 200:
+            return response
     
 
-    def get_api_data(self, url, fromCSV = False):
+    def get_api_data(self, url, fromCSV = False, encoding = "utf-8"):
+        response_content = self.get_api_response(url).content
         if not fromCSV:
-            response = self.get_api_response(url)
-            response_json = json.loads(response.content)
+            response_json = json.loads(response_content)
             return json_normalize(response_json)
         else:
-            pass
+            read_csv(io.StringIO(response_content.decode(encoding)))
     
 
-    def get_api_graph(url, file_ext = ".png"):
-        pass
+    def get_api_graph(self, url, file_ext = ".png"):
+        response = self.get_api_response(url)
+        img_file = TemporaryFile(suffix=file_ext).name
+        with open(img_file, 'wb') as f:
+            f.write(response.content)
+        return {'response': response, 'graph': img_file}
+
 
