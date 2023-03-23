61f88bed93c5ba06f7669de7af45509c14308d32
Azondekon <poj2@cdc.gov>
2023-03-23 17:24:22 -0400

    Update Token class definition

--- pynssp/core/token.py
+++ pynssp/core/token.py
@@ -3,7 +3,7 @@ import json
 import io
 from pandas import json_normalize, read_csv
 from cryptography.fernet import Fernet
-from pynssp.core.container import NSSPContainer
+from pynssp.core.container import NSSPContainer, APIGraph
 from tempfile import TemporaryFile
 
 class Token:
@@ -15,7 +15,7 @@ class Token:
     def get_api_response(self, url):
         headers = {
             'Authorization': "{} {}".
-            format(self.access_token, self.__k.decrypt(self.token.value.encode()))
+            format(self.access_token, self.__k.decrypt(self.token.value))
             }
         response = requests.get(url, headers=headers)
         print("{}: {}".format(response.status_code, response.reason))
@@ -28,11 +28,11 @@ class Token:
             response_json = json.loads(response_content)
             return json_normalize(response_json)
         else:
-            read_csv(io.StringIO(response_content.decode(encoding)))
+            return read_csv(io.StringIO(response_content.decode(encoding)))
 
     def get_api_graph(self, url, file_ext = ".png"):
         response = self.get_api_response(url)
         img_file = TemporaryFile(suffix=file_ext).name
         with open(img_file, 'wb') as f:
             f.write(response.content)
-        return {'response': response, 'graph': img_file}
+        return APIGraph(path=img_file, response=response)
