76a773239b5e8463299313e62e9dde259bcb8471
Azondekon <poj2@cdc.gov>
2023-04-20 10:47:31 -0400

    Dropping support for python 3.6

--- setup.py
+++ setup.py
@@ -20,14 +20,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Gbedegnon Roseric Azondekon",
     author_email='gazondekon@cdc.gov, roseric_2000@yahoo.fr',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
