0845dac5264738afd521a45b94083fee8c3bdba6
Azondekon <poj2@cdc.gov>
2023-04-27 16:16:43 -0400

    Update gh-pages.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,12 @@ import pynssp
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'm2r', 'sphinx_rtd_theme']
+extensions = [
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.viewcode', 
+    'm2r', 
+    'sphinx_rtd_theme',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -65,7 +70,7 @@ release = pynssp.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'English'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
