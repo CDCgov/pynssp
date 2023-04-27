4ddc4d45def98ffa70acfb7fb1e6422e946b8216
Azondekon <poj2@cdc.gov>
2023-04-27 19:56:40 -0400

    Update pynssp doc configurations

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ release = pynssp.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'English'
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -109,7 +109,22 @@ html_context = {
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'analytics_id': 'G-5FTLT9PCW7',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    # 'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
