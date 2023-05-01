f6568326238713b4261644ca87c520942e6f44cd
Azondekon <POJ2@cdc.gov>
2023-05-01 10:22:08 -0400

    Update docs confs, change pigment style to default

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ language = 'en'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default' #'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
