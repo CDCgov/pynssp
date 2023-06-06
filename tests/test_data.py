5af50614300a20a1e627736a4bdab17c51d54887
Azondekon <poj2@cdc.gov>
2023-06-06 16:39:52 -0400

    Add tests scripts for data module

---
+++ tests/test_data.py
@@ -0,0 +1,36 @@
+import pytest
+import pandas as pd
+from pynssp import load_simulated_ts, get_scenario1, get_scenario2
+
+
+@pytest.fixture
+def simulated_ts():
+    return load_simulated_ts()
+
+
+@pytest.fixture
+def scenario1():
+    return get_scenario1()
+
+
+@pytest.fixture
+def scenario2():
+    return get_scenario2()
+
+
+def test_load_simulated_ts(simulated_ts):
+    assert isinstance(simulated_ts, pd.DataFrame)
+    assert len(simulated_ts) == 626
+    assert len(simulated_ts.columns) == 6
+
+
+def test_get_scenario1(scenario1):
+    assert isinstance(scenario1, pd.DataFrame)
+    assert len(scenario1) == 313
+    assert len(scenario1.columns) == 6
+
+
+def test_get_scenario2(scenario2):
+    assert isinstance(scenario2, pd.DataFrame)
+    assert len(scenario2) == 313
+    assert len(scenario2.columns) == 6
