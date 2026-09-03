ae411c32ba495c68d4e2b9a07422cd60d0d1b988
Azondekon <poj2@cdc.gov>
2026-09-03 13:19:11 -0400

    update get_api_response unit test

--- tests/test_get_api_response.py
+++ tests/test_get_api_response.py
@@ -1,6 +1,6 @@
 import os
 import pytest
-from pynssp import get_api_response, Credentials, Token
+from pynssp import get_api_response, Credentials, Token, Apikey
 import requests
 
 
@@ -13,22 +13,31 @@ def profile():
 def tokenProfile():
     return Token("abc1234567890")
 
+@pytest.fixture
+def apikeyProfile():
+    return Apikey(" ", " ")
 
-def test_profile_class(profile, tokenProfile):
+
+def test_profile_class(profile, tokenProfile, apikeyProfile):
     assert isinstance(profile, Credentials)
     assert isinstance(tokenProfile, Token)
+    assert isinstance(apikeyProfile, Apikey)
 
 
-def test_profile_save(profile, tokenProfile):
+def test_profile_save(profile, tokenProfile, apikeyProfile):
     profile.pickle()
     tokenProfile.pickle()
+    apikeyProfile.pickle()
     profile.pickle(file="test.pkl")
     tokenProfile.pickle(file="test2.pkl")
+    apikeyProfile.pickle(file="test3.pkl")
 
     assert os.path.isfile("myProfile.pkl")
     assert os.path.isfile("tokenProfile.pkl")
+    assert os.path.isfile("apikeyProfile.pkl")
     assert os.path.isfile("test.pkl")
     assert os.path.isfile("test2.pkl")
+    assert os.path.isfile("test3.pkl")
 
     test_files = os.listdir(".")
 
@@ -42,12 +51,15 @@ def test_get_api_response():
 
     handle = Credentials(" ", " ")
     handle2 = Token("abc1234567890")
+    handle3 = Apikey(" ", " ")
 
     response = get_api_response(url, profile=handle)
     response2 = get_api_response(url, profile=handle2)
+    response3 = get_api_response(url, profile=handle3)
 
     assert isinstance(response, requests.models.Response)
     assert isinstance(response2, requests.models.Response)
+    assert isinstance(response3, requests.models.Response)
 
     with pytest.raises(Exception):
         get_api_response(url, profile=[])
